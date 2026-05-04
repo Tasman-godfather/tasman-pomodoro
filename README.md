db032639f7302fc7a5d47463a8d66d64f6d6ef02
Tasman-godfather <60338137+Tasman-godfather@users.noreply.github.com>
2026-05-04 11:35:07 +0800

    Publish Tasman Pomodoro prototype

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tasman Pomodoro
+
+Installable iOS-style prototype for Tasman 番茄钟.
+
+## GitHub Pages
+
+Publish the repository with GitHub Pages from the `main` branch and root folder.
+
+Entry file: `index.html`
+
---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'tasman-pomodoro-prototype-v1';
+const APP_SHELL = [
+  './index.html',
+  './manifest.webmanifest',
+  './app-icon.svg',
+  './app-icon-180.png',
+  './app-icon-512.png',
+  'https://unpkg.com/react@18.2.0/umd/react.development.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone@7.24.7/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request).then((response) => {
+        const responseCopy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+        return response;
+      });
+    })
+  );
+});
